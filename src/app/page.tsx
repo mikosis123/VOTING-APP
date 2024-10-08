a82465f957fb6ea4504eb1834e3ab31ee587f417
mikosis123 <mikosis93@gmail.com>
2024-10-08 22:06:13 +0300

    move proposals to proposals page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,23 @@
 "use client";
-import { useReadContract, useSendTransaction } from "thirdweb/react";
+import { useReadContract } from "thirdweb/react";
 import { contract } from "./client";
 import { ConnectButton } from "thirdweb/react";
 import { client } from "./client";
 import { useActiveAccount, useWalletBalance } from "thirdweb/react";
 import { defineChain } from "thirdweb/chains";
-import Proposals from "./components/Proposals";
 import AddMember from "./components/AddMember";
-import { prepareContractCall, stringToBytes } from "thirdweb";
+
 import { useState } from "react";
-import { Buffer } from "buffer";
-import Nav from "./components/Nav";
+
 export default function Home() {
   const account = useActiveAccount();
   const chain = defineChain(11155111); // Assuming this is Sepolia testnet.
-  const [newProposal, setNewProposal] = useState("");
-  const { mutate: sendTransaction } = useSendTransaction();
+
   const { data: balance, isLoading: isBalanceLoading } = useWalletBalance({
     client,
     chain: chain,
     address: account?.address,
   });
-
-  const encodedData = stringToBytes(newProposal);
-  const hexEncodedData = `0x${Buffer.from(encodedData).toString(
-    "hex"
-  )}` as `0x${string}`;
-  console.log(hexEncodedData); // Already returns `bytes`
-
-  const onClick = async () => {
-    if (!account?.address) {
-      console.error("No account connected");
-      return;
-    }
-
-    try {
-      const transaction = await prepareContractCall({
-        contract,
-        method: "function NewProposal(address proposal, bytes data)",
-        params: [account.address, hexEncodedData], // Pass address and encoded data
-      });
-
-      sendTransaction(transaction, {
-        onSuccess: (txResult) => {
-          console.log("Transaction successful!", txResult);
-        },
-        onError: (error) => {
-          console.error("Transaction failed:", error);
-        },
-      });
-    } catch (error) {
-      console.error("Error preparing transaction:", error);
-    }
-  };
 
   const { data: ownerAddress, isPending: isOwnerPending } = useReadContract({
     contract,
@@ -101,23 +66,6 @@ export default function Home() {
           {account && (
             <div>
               <AddMember />
-              <input
-                value={newProposal}
-                onChange={(e) => setNewProposal(e.target.value)}
-                type="text"
-                className="border border-gray-300 rounded-md p-2 m-2"
-                id="proposal"
-                name="proposal"
-                placeholder="Enter proposal data"
-              />
-
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={onClick}
-                // Disable button while sending
-              >
-                Creating Proposal
-              </button>
 
               <div className="grid gap-4 justify-center mt-10">
                 <p>Wallet Address: {account?.address}</p>
@@ -141,8 +89,6 @@ export default function Home() {
                 ) : (
                   <p>Is Member: {members ? "Yes" : "No"}</p>
                 )}
-
-                <Proposals />
               </div>
             </div>
           )}
