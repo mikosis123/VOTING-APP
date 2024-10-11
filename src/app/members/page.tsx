0e024b7888a9179aa2b0c0cbd077dca519c2fc8e
mikosis123 <mikosis93@gmail.com>
2024-10-11 18:44:10 +0300

    members list

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,54 @@
+"use client";
 import React from "react";
+import { useReadContract } from "thirdweb/react";
+import { contract } from "../client";
 
 const Members = () => {
-  return <div></div>;
+  const { data, isPending } = useReadContract({
+    contract,
+    method:
+      "function getAllMembers() view returns ((address memberAddress, string name)[])",
+    params: [],
+  });
+
+  if (isPending) {
+    return <p>Loading members...</p>;
+  }
+
+  if (data && data.length > 0) {
+    // Filter out repeated members by address or name
+    const uniqueMembers = Array.from(
+      new Set(
+        data.map((member: { memberAddress: string; name: string }) =>
+          JSON.stringify(member)
+        )
+      )
+    ).map((memberString) => JSON.parse(memberString));
+
+    return (
+      <div className="relative min-h-screen w-full bg-slate-950">
+        <div className="absolute inset-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]">
+          <h2>Members List</h2>
+          {uniqueMembers.map(
+            (
+              member: { memberAddress: string; name: string },
+              index: number
+            ) => (
+              <div
+                key={index}
+                className="flex justify-between items-center w-[80%] mx-auto border border-zinc-800 p-8 mt-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
+              >
+                <p>Address: {member.memberAddress}</p>
+                <p>Name: {member.name}</p>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return <div>No members found.</div>;
 };
 
 export default Members;
