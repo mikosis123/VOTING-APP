5c75b8d51d5f1bb65f515a7b94176eab2edace54
mikosis123 <mikosis93@gmail.com>
2024-10-11 18:42:53 +0300

    adding style change

--- src/app/components/AddMember.tsx
+++ src/app/components/AddMember.tsx
@@ -6,23 +6,35 @@ import { contract } from "../client";
 const AddMember = () => {
   const { mutate: sendTransaction } = useSendTransaction();
   const [_newMember, setNewMember] = React.useState("");
+  const [_name, setName] = React.useState("");
   console.log(_newMember);
   const onClick = () => {
     const transaction = prepareContractCall({
       contract,
-      method: "function addMember(address _newMember)",
-      params: [_newMember],
+      method: "function addMember(address _newMember, string _name)",
+      params: [_newMember, _name],
     });
     sendTransaction(transaction);
+    setNewMember("");
+    setName("");
   };
   return (
     <div>
       <input
         onChange={(e) => setNewMember(e.target.value)}
         type="text"
-        className="border border-gray-300 rounded-md p-2 m-2"
+        className="border text-black border-gray-300 rounded-md p-2 m-2"
+        placeholder="address"
         id="newMember"
         name="newMember"
+      />
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        className="border text-black border-gray-300 rounded-md p-2 m-2"
+        id="isname"
+        placeholder="name"
+        name="isname"
       />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
